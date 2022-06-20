11796314b7e9286ae560bd17067475cf96058a7f
Dmitriy Meshkov <dimkameshkov@gmail.com>
2022-06-20 22:07:10 +0500

    Задача: проверить является ли первое число квадратом второго.Выводит сумму вводимого числа с числом 9;

---
+++ Seminar1_First_Task/Program.cs
@@ -0,0 +1,8 @@
+﻿
+//Выводит сумму вводимого числа с числом 9;
+int number;
+
+Console.WriteLine("Input a number: ");
+number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Your number is {number}");  //Console.Write($"Your number is {number}");
+Console.WriteLine($"My Sum is {number + 9} and it is good"); //Console.WriteLine("My number is " + number + " and it is good");
