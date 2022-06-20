670b7f77f358acfb06e7aa391c5692bf702f9b49
Dmitriy Meshkov <dimkameshkov@gmail.com>
2022-06-20 23:30:07 +0500

    программа принимает число n и выводит целые числа от -n до n

---
+++ Seminar1_Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿//программа принимает число n и выводит целые числа от -n до n
+int n;
+int current;
+Console.Write("Input a number: "); // Вывод текста в консоль;
+n = Convert.ToInt32(Console.ReadLine());
+
+if(n<0)
+{
+    n = n*(-1);
+}
+current = n * (-1);
+while(current<= n)
+{
+    Console.Write(current + " ");
+    current++;//+=1
+}
